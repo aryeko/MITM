6c94972823159115b9e4c9b3242ac59ba80a6066
Liran BG <liranbg@gmail.com>
2015-06-13 18:48:13 +0300

    changed notifications in Bobs Client

--- Client.py
+++ Client.py
@@ -98,37 +98,37 @@ class Bob(Client):
         print "Listening..."
         self.sock.listen(10)
         print "Accepting..."
+
         conn, address_info = self.sock.accept()
-        print 'Connected with ' + address_info[0] + ':' + str(address_info[1])
+        print 'Connected with ' + address_info[0] + ':' + str(address_info[1]), "===================================="
         while True:
             data = conn.recv(1024)
             if not data:
                 conn.close()
                 return
             if data == "Hey I'm Alice":
-                print "Waving To Alice"
+                print "Waved To Alice"
                 self.send_message(conn, "Hey I'm Bob")
 
-            elif data == "GET PUBLIC KEY":
-                print "Sending Alice My Public Key"
-                self.send_public_key(conn)
-
             elif '-----BEGIN PUBLIC KEY-----' in data:
-                print "Getting Alice's Public Key"
+                print "Received Alice's Public Key"
                 self.dst_public_key = RSA.importKey(str(data))
 
             elif "QUIT" == data:
                 self.sock.close()
                 break
 
+            elif data == "GET PUBLIC KEY":
+                print "Sent Alice My Public Key"
+                self.send_public_key(conn)
+
             elif self.read_enc_message(data) == "GET PWD":
-                print "received GET PWD"
+                print "Received GET PWD"
                 self.send_enc_message(conn, self.alice_password)
 
             elif self.read_enc_message(data) == "SET PWD":
-                print "received SET PWD"
+                print "Received SET PWD"
                 self.send_message(conn, "OK")
-                print "Setting new Alice's password..."
                 self.alice_password = self.read_enc_message(conn.recv(1024))
                 print "Alice's password is: " + self.alice_password
 
